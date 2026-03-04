1c7c36f48fadb2da7ebda76d413fd9fe8526a757
dafadaifullah <dafadaifullah72@gmail.com>
2026-03-04 11:37:45 +0700

    Add files via upload

---
+++ pelanggan/pelanggan/src/main/java/com/dafa/pelanggan/repository/PelangganRepository.java
@@ -0,0 +1,11 @@
+package com.dafa.pelanggan.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dafa.pelanggan.model.Pelanggan;
+
+@Repository
+public interface PelangganRepository extends JpaRepository<Pelanggan, Long> {
+
+}
