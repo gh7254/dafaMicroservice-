c3cc1fe0bfbedd9ae34b899682cd6ba41b6af892
gh7254 <dafadaifullah54@gmail.com>
2026-05-12 08:35:05 +0700

    Add files via upload

---
+++ microservice/authorization/src/main/java/com/dafa/authorization/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.dafa.authorization.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.dafa.authorization.dto.AuthRequest;
+import com.dafa.authorization.dto.AuthResponse;
+import com.dafa.authorization.service.AuthService;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    // Contoh endpoint yang diproteksi JWT
+    @GetMapping("/test-auth")
+    public ResponseEntity<String> testEndpoint() {
+        return ResponseEntity.ok("Halo! Jika kamu melihat pesan ini, berarti Token JWT kamu valid.");
+    }
+}
