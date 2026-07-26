68598d6bfb41e5345171930e904c78020561d299
dafadaifullah <dafadaifullah54@gmail.com>
2026-07-26 21:39:08 +0700

    Add files via upload

---
+++ microservice/monitoring/produk/src/test/java/com/dafa/produk/ProdukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dafa.produk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
